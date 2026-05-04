1b7b68a50aef897432b597c9e7464896ad6b1306
rhavymaia <rmg3@cin.ufpe.br>
2026-05-04 16:55:01 -0300

    Validação e modularização.

--- models/Avicultor.py
+++ models/Avicultor.py
@@ -1,10 +1,20 @@
+from marshmallow import Schema, fields, validate
+
+
 class Avicultor():
-    def __init__(self, nome, nascimento, cpf, caf):
+    def __init__(self, id, nome, nascimento, cpf, caf):
+        self.id = id
         self.nome = nome
         self.nascimento = nascimento
         self.cpf = cpf
         self.caf = caf
 
     def toDict(self):
-        return {"nome": self.nome, "nascimento": self.nascimento,
-                "cpf": self.cpf, "caf": self.caf}
+        return {"id": self.id, "nome": self.nome, "nascimento": self.nascimento, "cpf": self.cpf, "caf": self.caf}
+
+
+class AvicultorSchema(Schema):
+    nome = fields.Str(required=True)
+    nascimento = fields.Date(required=True)
+    cpf = fields.Str(required=True, validate=validate.Length(max=11))
+    caf = fields.Str(required=True)
